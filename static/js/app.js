b5d06e9574a8718cca2cc9a342420ec2199da0a8
Andrew Allbright <aallbrig@gmail.com>
2020-12-03 11:08:27 -0500

    User is able to reset processor app state

--- static/js/app.js
+++ static/js/app.js
@@ -1,46 +1,65 @@
-const LOCAL_STORAGE_APP_KEY = 'app';
+const LOCAL_STORAGE_APP_KEY = 'app-state';
+// TODO: storage any reports generated user in local storage
+const LOCAL_STORAGE_REPORTS_KEY = 'app-reports';
+
+class ReportReports {
+  constructor() {
+    this.dom = {};
+    const storedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_REPORTS_KEY));
+    const stateDefaults = {};
+    this.state = Object.assign({}, stateDefaults, storedState);
+  }
+}
+
 class ProcessRepairVideosApp {
   constructor() {
-    // TODO: rehydrate app with local storage data
-    const storageState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_APP_KEY));
-    const stateDefaults = {
-      hideTopSection: false,
-      showProcessingForm: false,
-    };
-
-    this.state = Object.assign({}, stateDefaults, storageState);
-
     this.dom = {
       topSection: document.getElementById('information-collapsable'),
       hideTopBtn: document.getElementById('hide-top'),
       assignVideoBtn: document.getElementById('assign-video'),
+      resetAppBtn: document.getElementById('reset-app'),
       resetVideoBtn: document.getElementById('reset-video'),
       videoTitle: document.getElementById('video-title'),
       videoIframe: document.getElementById('video-iframe'),
       processGuide: document.getElementById('process-guide'),
       waitingText: document.getElementById('waiting'),
+      reportForm: document.getElementById('repair-report-form'),
     }
 
+    const storedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_APP_KEY));
+    const stateDefaults = {
+      hideTopSection: false,
+      showProcessingForm: false,
+    };
+
+    this.state = Object.assign({}, stateDefaults, storedState);
+
+
+    this.dom.resetAppBtn.onclick = this.resetAppState.bind(this);
     this.dom.resetVideoBtn.onclick = this.resetProcessForm.bind(this);
     this.dom.hideTopBtn.onclick = () => {
       this.setState({ hideTopSection: true });
     };
 
-    if (this.state.video) {
-      this.resetProcessForm();
-      this.renderProcessVideoHtml(this.state.video);
-      this.showProcessForm();
-    }
-
-    if (this.state.hideTopSection) {
-      this.dom.topSection.classList.remove('show');
-    }
+    this.renderState(this.state);
   }
 
   setState(newState) {
     this.state = Object.assign({}, this.state, newState);
 
     window.localStorage.setItem(LOCAL_STORAGE_APP_KEY, JSON.stringify(this.state));
+  }
+
+  renderState(state) {
+    if (state.video) {
+      this.resetProcessForm();
+      this.renderProcessVideoHtml(this.state.video);
+      this.showProcessForm();
+    }
+
+    if (state.hideTopSection) {
+      this.dom.topSection.classList.remove('show');
+    }
   }
 
   renderProcessVideoHtml(videoDataRow) {
@@ -51,12 +70,19 @@ class ProcessRepairVideosApp {
   }
 
   resetProcessForm() {
+    // TODO: Reset form data
+    const formData = Object.fromEntries(new FormData(this.dom.reportForm).entries());
     // this.setState({ showProcessingForm: false });
     // this.dom.waitingText.classList.remove('d-none');
     // this.dom.processGuide.classList.add('d-none');
     // this.dom.videoTitle.innerText = '';
     // this.dom.videoIframe.src = '';
     this.dom.resetVideoBtn.classList.add('disabled');
+  }
+
+  resetAppState() {
+    window.localStorage.removeItem(LOCAL_STORAGE_APP_KEY);
+    window.location.reload();
   }
 
   showProcessForm() {
