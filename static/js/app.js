638251a8c00cfbdec83d58519d9a9c21cb185ac5
Andrew Allbright <aallbrig@gmail.com>
2020-12-03 10:20:15 -0500

    User can input mac info

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,79 @@
-// TODO: Save current video and form inputs in local storage
-const assignVideoBtn = document.getElementById('assign-video');
-const resetVideoBtn = document.getElementById('reset-video');
-const videoTitle = document.getElementById('video-title');
-const videoIframe = document.getElementById('video-iframe');
-const processGuide = document.getElementById('process-guide');
-const waitingText = document.getElementById('waiting');
+class ProcessRepairVideosApp {
+  constructor() {
+    // TODO: rehydrate app with local storage data
+    this.state = Object.assign({}, {
+      showTopSection: true,
+      showProcessingForm: false,
+    });
 
-async function renderProcessVideoHtml(videoDataRow) {
-  videoTitle.innerText = videoDataRow.Video.Title;
-  // Disallow browser URL caching
-  // TODO: Is this a bad idea? What about offline experince?
-  videoIframe.src = `${videoDataRow.Video.EmbeddedUrl}?timestamp=${new Date().getTime()}`;
-}
+    this.dom = {
+      assignVideoBtn: document.getElementById('assign-video'),
+      resetVideoBtn: document.getElementById('reset-video'),
+      videoTitle: document.getElementById('video-title'),
+      videoIframe: document.getElementById('video-iframe'),
+      processGuide: document.getElementById('process-guide'),
+      waitingText: document.getElementById('waiting'),
+    }
 
-async function reset() {
-  videoTitle.innerText = '';
-  videoIframe.src = '';
+    this.dom.resetVideoBtn.onclick = this.resetProcessForm.bind(this);
+  }
+
+  setState(newState) {
+    this.state = Object.assign({}, this.state, newState);
+    // TODO: Save current video and form inputs in local storage
+  }
+
+  renderProcessVideoHtml(videoDataRow) {
+    this.dom.videoTitle.innerText = videoDataRow.Video.Title;
+    // Disallow browser URL caching so that iframe does not
+    // TODO: Is this a bad idea? What about offline experince?
+    this.dom.videoIframe.src = `${videoDataRow.Video.EmbeddedUrl}?timestamp=${new Date().getTime()}`;
+  }
+
+  resetProcessForm() {
+    this.setState({ showProcessingForm: false });
+    this.dom.waitingText.classList.remove('d-none');
+    this.dom.processGuide.classList.add('d-none');
+    this.dom.videoTitle.innerText = '';
+    this.dom.videoIframe.src = '';
+    this.dom.resetVideoBtn.classList.add('disabled');
+  }
+
+  showProcessForm() {
+    this.setState({ showProcessingForm: true });
+    this.dom.waitingText.classList.add('d-none');
+    this.dom.processGuide.classList.remove('d-none');
+    this.dom.resetVideoBtn.classList.remove('disabled');
+  }
+
+  setRepairVideoData(repairVideos) {
+    const processQueue = repairVideos.filter(_ => _.NeedsProcessing);
+
+    this.dom.assignVideoBtn.onclick = () => {
+      const randomVideo = processQueue[Math.ceil(Math.random() * processQueue.length)];
+      this.resetProcessForm();
+      this.renderProcessVideoHtml(randomVideo);
+      this.showProcessForm();
+    };
+
+    this.dom.assignVideoBtn.classList.remove('disabled');
+  }
 }
 
 async function main() {
-  const jsonRequest = await fetch('json/repair-videos-data.json');
-  const json = await jsonRequest.json();
-  const processQueue = json.filter(_ => _.NeedsProcessing);
-
-  assignVideoBtn.onclick = async () => {
-    const randomVideo = processQueue[Math.ceil(Math.random() * processQueue.length)];
-    waitingText.classList.remove('d-none');
-    processGuide.classList.add('d-none');
-    await renderProcessVideoHtml(randomVideo);
-    processGuide.classList.remove('d-none');
-
-    resetVideoBtn.classList.remove('btn-secondary', 'disabled');
-    resetVideoBtn.classList.add('btn-primary');
-    waitingText.classList.add('d-none');
-  };
-
-  resetVideoBtn.onclick = async () => {
-    waitingText.classList.remove('d-none');
-    processGuide.classList.add('d-none');
-    resetVideoBtn.classList.remove('btn-primary');
-    resetVideoBtn.classList.add('btn-secondary', 'disabled');
-    await reset();
-  };
-  assignVideoBtn.classList.remove('disabled');
+  const app = new ProcessRepairVideosApp();
+  const getRepairDataReq = await fetch('json/repair-videos-data.json');
+  const repairDataJson = await getRepairDataReq.json();
+  app.setRepairVideoData(repairDataJson);
 }
 
 try {
-  const _ = main();
+  const program = main();
+  program.catch((e) => {
+    // start app anyways
+    const app = new ProcessRepairVideosApp();
+    app.setRepairVideoData([]);
+  });
 } catch (e) {
   alert(e);
 }
